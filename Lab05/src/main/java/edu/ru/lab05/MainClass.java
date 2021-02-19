d30c0d6d2138ecaad9416281cc0d19ca09633159
Maria Palacios <mariapalacios5889@gmail.com>
2021-02-19 12:22:41 -0600

    added code and tested

--- Lab05/src/main/java/edu/ru/lab05/MainClass.java
+++ Lab05/src/main/java/edu/ru/lab05/MainClass.java
@@ -7,14 +7,26 @@ public class MainClass {
     
     public static void main(String[] args){
         
-        System.out.println("hello world"); 
+       String word = "racecar";
+        System.out.println("The word '"+ word + "' is a palindrome: ");
+        System.out.println(isPalindrome(word));
         
     }
     
     
     public static boolean isPalindrome(String word){
         
+        int i = 0;
+        int j = word.length()-1;
         
+        while (j>i){
+            if(word.charAt(i) != word.charAt(j)){
+                return false;
+            }
+            i = i + 1;
+            j = j - 1;
+            
+        }
         
         return true;
     }
