dce7234d44eca270a96835ecabdd8bd214bf2c92
Maria Palacios <mariapalacios5889@gmail.com>
2021-02-19 12:15:07 -0600

    adding project file

---
+++ Lab05/src/main/java/edu/ru/lab05/MainClass.java
@@ -0,0 +1,21 @@
+
+package edu.ru.lab05;
+
+
+public class MainClass {
+   
+    
+    public static void main(String[] args){
+        
+        System.out.println("hello world"); 
+        
+    }
+    
+    
+    public static boolean isPalindrome(String word){
+        
+        
+        
+        return true;
+    }
+}
